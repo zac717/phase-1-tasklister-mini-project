446d782a72b8a30b64e02f8d755937adaf442daa
isaac <isaacongera717@gmail.com>
2022-12-12 07:54:30 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleTask(e.target.description.value)
+    form.reset()
+  })
 });
+
+function handleTask(task){
+
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = "x";
+  btn.addEventListener('click', handleDelete)
+
+  li.textContent = `${task} `;
+  li.appendChild(btn)
+
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
